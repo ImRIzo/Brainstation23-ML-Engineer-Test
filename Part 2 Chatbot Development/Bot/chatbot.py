1cccf8c15c0732596e530b0c7d00dfc50e841282
Rizwan <rizowan69@gmail.com>
2024-04-21 22:16:50 +0600

    bot screenshot added

--- Part 2 Chatbot Development/Bot/chatbot.py
+++ Part 2 Chatbot Development/Bot/chatbot.py
@@ -1,18 +1,30 @@
-#import database
+import database
+import random
+
+grettings_list = ["hi", "Hi","HI", "Hello"]
+
+query_list = ["From which country do we have the most customers?",
+              "Most customers country wise?",
+                "richest people"  
+
+            ]
 
 
-grettings_list = ["hi", "Hi","HI", "How are you?"]
-query_list = ["hi", "Hi","HI", "How are you?"]
+grettings_answer_random = ["I am good, How are you?", "I am fine, How can I help you?","Can't complain !"]
 
 class Medusa:
     def __init__(self) -> None:
         pass
     
     def enquiry(self, user_text:str):
-        if user_text in grettings_list:
-            return "Hello, how are you?"
+        if user_text == "How are you?":
+            return random.choice(grettings_answer_random)
+        
+        elif user_text in grettings_list:
+            return " Hey there! How are yout today? "
+
         elif user_text in query_list:
-            
-            return "sd"
+            text = database.get_most_order_countrywise()            
+            return f'We have the most customers from {text}. 🔝' 
         else:
             return "Sorry, I didn't understand that."
